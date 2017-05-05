44f28222887788aa50e8c18752da16c6f26ba8fe
Roman Golubev <grom272008@yandex.ru>
2017-05-05 07:19:14 +0300

    Выполнил три задания lesson-2

--- lesson-2/fuctions.php
+++ lesson-2/fuctions.php
@@ -1,14 +1,14 @@
 <?php
 
+// 2. Функция вычисления дискриминанта квадратного уравнения
 function discriminant($b, $a, $c)
 {
-    return $b**2 - 4*$a*$c;
+    return $b ** 2 - 4 * $a * $c;
 }
 
-assert(9 == discriminant( 5, 2, 2));
-assert(0 == discriminant( 4, 1, 4));
-assert(-39 == discriminant( 3, 4, 3));
-
+assert(9 == discriminant(5, 2, 2));
+assert(0 == discriminant(4, 1, 4));
+assert(-39 == discriminant(3, 4, 3));
 
 //var_dump(discriminant(2, 5, 2));
 
@@ -17,6 +17,23 @@ assert(-39 == discriminant( 3, 4, 3));
 function returnInclude()
 {
     return include __DIR__ . '/include.php';
+}
+
+// 4. Составьте функцию, которая на вход будет принимать имя человека, а возвращать его пол, пытаясь угадать по имени
+// (null - если угадать не удалось). Вам придется самостоятельно найти нужные вам строковые функции. Начните с написания тестов для функции.
+// http://fi2.php.net/manual/ru/ref.strings.php
+
+// Руслан, Роман, Андрей, Сергей, Павел,
+// Людмила, Ирина, Катерина, Анастасия, Светлана, Дарья, Мария, Маргарита
+
+$genderMan = 'Мужской';
+$genderWoman = 'Женский';
+
+$mens = 1;
+
+function gender($name)
+{
+    return $gender;
 }
 
 
--- lesson-2/lesson-2.php
+++ lesson-2/lesson-2.php
@@ -4,6 +4,7 @@
         <section class="container-center">
             <h1>Выполнение домашнего задания урока 2&nbsp;курса &laquo;PHP-1&raquo;</h1>
 
+            <!-- Задание 1. Таблица истинности -->
             <article class="table-true">
                 <h3>1. Напишите программу, которая составит и выведет в браузер таблицу истинности для логических
                     операторов
@@ -64,18 +65,20 @@
                     </tr>
                 </table>
             </article>
+            <!-- /Задание 1. Таблица истинности -->
 
+            <!-- Задание 2. Вычисление дискриминанта квадратного уравнения -->
             <article class="equation">
                 <h3>2. Составьте функцию вычисления дискриминанта квадратного уравнения. Покройте ее тестами. Используя
                     эту функцию, напишите программу, которая решает квадратное уравнение. Оформите красивый вывод
                     решения.</h3>
 
-                <p>Дискриминант квадратного уравнения вычисляется по формуле: D = b<sup>2</sup> - 4ac</p>
+                <p>Дискриминант квадратного уравнения: D = b<sup>2</sup> - 4ac</p>
 
                 <?php
-                $a = 2;
-                $b = 5;
-                $c = 2;
+                $a = 1;
+                $b = 2;
+                $c = 1;
                 $D = discriminant($b, $a, $c);
                 ?>
 
@@ -83,78 +86,106 @@
                     <li>a = <?php echo $a; ?></li>
                     <li>b = <?php echo $b; ?></li>
                     <li>c = <?php echo $c; ?></li>
-                    <li>Дискриминант D = <?php echo $D; ?></li>
+                    <li>Дискриминант D = <?php echo $b; ?><sup>2</sup> -
+                        4&times;<?php echo $a; ?>&times;<?php echo $c; ?> = <?php echo $D; ?></li>
                 </ul>
 
-                <table>
-                    <tr>
-                        <td rowspan="2">x<sub>1,2</sub> =</td>
-                        <td class="downline">-(<?php echo $b; ?>) &#177; &#8730;<span class="upline">(<?php echo $b; ?>)<sup>2</sup> - 4 &#215; <?php echo $a; ?> &#215; <?php echo $c; ?></span></td>
-                        <td rowspan="2"> = </td>
-                        <td class="downline">8 &#177; &#8730;<span class="upline">64 - 48</span></td>
-                        <td rowspan="2"> = </td>
-                        <td class="downline">8 &#177; &#8730;<span class="upline"><?php echo $D; ?></span></td>
-                        <td rowspan="2"> = </td>
-                        <td class="downline">8 &#177; 4</td>
-                    </tr>
-                    <tr class="downnumber">
-                        <td>2 &#215; <?php echo $a; ?></span></td>
-                        <td>6</td>
-                        <td>6</td>
-                        <td>6</td>
-                    </tr>
-                </table>
-                <table>
-                    <tr>
-                        <td rowspan="2">x<sub>1</sub> =</td>
-                        <td class="downline">8 + 4</td>
-                        <td rowspan="2"> = </td>
-                        <td class="downline">12</td>
-                        <td rowspan="2"> = 2</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>6</td>
-                    </tr>
-                </table>
-                <table>
-                    <tr>
-                        <td rowspan="2">x<sub>2</sub> =</td>
-                        <td class="downline">8 - 4</td>
-                        <td rowspan="2"> = </td>
-                        <td class="downline">4</td>
-                        <td rowspan="2"> = <?php echo 4 / 6; ?></td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>6</td>
-                        <td></td>
-                    </tr>
-                </table>
-
                 <?php
-
                 switch ($D) {
                     case ($D > 0):
+                        $x1 = (-$b + sqrt($D)) / (2 * $a);
+                        $x2 = (-$b - sqrt($D)) / (2 * $a);
+                        ?>
+
+                        <div class="equation__table">
+                            <table>
+                                <tr>
+                                    <td rowspan="2">x<sub>1,2</sub> =</td>
+                                    <td class="downline">-(<?php echo $b; ?>) &#177; &#8730;<span
+                                            class="upline">(<?php echo $b; ?>)<sup>2</sup> - 4 &#215; <?php echo $a; ?>
+                                            &#215; <?php echo $c; ?></span></td>
+                                </tr>
+                                <tr class="downnumber">
+                                    <td>2 &#215; <?php echo $a; ?></span></td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td rowspan="2">x<sub>1</sub> =</td>
+                                    <td class="downline">-<?php echo $b; ?> + &#8730;<span
+                                            class="upline"><?php echo $D; ?></td>
+                                    <td rowspan="2"> = <?php echo $x1; ?> </td>
+                                </tr>
+                                <tr class="downnumber">
+                                    <td>2&times;<?php echo $a; ?></td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td rowspan="2">x<sub>2</sub> =</td>
+                                    <td class="downline">-<?php echo $b; ?> - &#8730;<span
+                                            class="upline"><?php echo $D; ?></td>
+                                    <td rowspan="2"> = <?php echo $x2; ?> </td>
+                                </tr>
+                                <tr class="downnumber">
+                                    <td>2&times;<?php echo $a; ?></td>
+                                </tr>
+                            </table>
+                        </div>
+
+                        <?php
                         break;
-                    case ($D == 0):
+                    case (0 == $D):
+                        $x = (-$b + sqrt($D)) / (2 * $a);
+                        ?>
+
+                        <div class="equation__table">
+                            <table>
+                                <tr>
+                                    <td rowspan="2">x<sub>1,2</sub> =</td>
+                                    <td class="downline">-(<?php echo $b; ?>) &#177; &#8730;<span
+                                            class="upline">(<?php echo $b; ?>)<sup>2</sup> - 4 &#215; <?php echo $a; ?>
+                                            &#215; <?php echo $c; ?></span></td>
+                                </tr>
+                                <tr class="downnumber">
+                                    <td>2 &#215; <?php echo $a; ?></span></td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td rowspan="2">x<sub>1</sub> =</td>
+                                    <td class="downline">-<?php echo $b; ?> + &#8730;<span
+                                            class="upline"><?php echo $D; ?></td>
+                                    <td rowspan="2"> = <?php echo $x1; ?> </td>
+                                </tr>
+                                <tr class="downnumber">
+                                    <td>2&times;<?php echo $a; ?></td>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <td rowspan="2">x<sub>2</sub> =</td>
+                                    <td class="downline">-<?php echo $b; ?> - &#8730;<span
+                                            class="upline"><?php echo $D; ?></td>
+                                    <td rowspan="2"> = <?php echo $x2; ?> </td>
+                                </tr>
+                                <tr class="downnumber">
+                                    <td>2&times;<?php echo $a; ?></td>
+                                </tr>
+                            </table>
+                        </div>
+
+                        <?php
                         break;
                     default:
+                        echo 'Корней нет';
                         break;
                 }
-
                 ?>
-
-
-
-
-
-
-
-
-
             </article>
+            <!-- /Задание 2. Вычисление дискриминанта квадратного уравнения -->
 
+            <!-- Задание 3. include как функция -->
             <article class="include">
                 <h3>3. Проведите самостоятельное исследование на тему "Что возвращает оператор include, если его
                     использовать как функцию?". Используйте руководство по языку, составьте и приложите примеры,
@@ -171,7 +202,18 @@
                 <p>В случае, когда файла нет, выводит предупреждение, что файла не
                     существует: <?php include __DIR__ . '/include_delete.php'; ?></p>
             </article>
+            <!-- /Задание 3. include как функция -->
 
+            <!-- Задание 4. Угадать пол по имени -->
+            <article class="gender">
+                <h3>4. Составьте функцию, которая на вход будет принимать имя человека, а возвращать его пол, пытаясь
+                    угадать по имени (null - если угадать не удалось). Вам придется самостоятельно найти нужные вам
+                    строковые функции. Начните с написания тестов для функции.</h3>
+
+                <p>Мужской</p>
+                <p>Женский</p>
+            </article>
+            <!-- /Задание 4. Угадать пол по имени -->
         </section>
     </main>
 
