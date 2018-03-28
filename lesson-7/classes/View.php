f7746fd7b72daf3b7e4a4b5902fafb9157ba6a51
Roman Golubev <grom272008@yandex.ru>
2018-03-28 22:48:27 +0400

    Добавление класса View

---
+++ lesson-7/classes/View.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+Создайте объект класса View, удовлетворяющий следующим требованиям:
+   • Конструктор не имеет аргументов (а может быть вообще не нужен никакой конструктор)
+   • Есть метод assign($name, $value), чья задача - сохранить данные, передаваемые в шаблон по заданному имени (используйте защищенное свойство - массив для хранения этих данных)
+   • Есть метод display($template), который отображает указанный шаблон с заранее сохраненными данными
+   • * Метод render($template), который аналогичен методу display(), но не выводит шаблон с данными в браузер, а возвращает его
+*/
+
+class View
+{
+    protected $data = [];
+
+    public function assign(string $name, $value)
+    {
+        $this->data[$name] = $value;
+        return $this;
+    }
+
+    public function display(string $template)
+    {
+        include $template;
+    }
+
+    public function render(string $template)
+    {
+        ob_start();
+        include $template;
+        //include __DIR__ . '/../templates/guestBookRecord.php';
+        $content = ob_get_contents();
+        ob_end_clean();
+
+        return $content;
+    }
+
+}
